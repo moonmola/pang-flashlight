4cc0cc39db28a67479981e5f8106f133534fc1c6
문인주 <miz722@khu.ac.kr>
2022-03-14 23:51:39 +0900

    add icon, flashlight

--- app/src/main/java/com/moonmola/pang_flashlight/MainActivity.kt
+++ app/src/main/java/com/moonmola/pang_flashlight/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.moonmola.pang_flashlight
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.moonmola.pang_flashlight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.switchButton.setOnClickListener{
+            binding.switchButton.isSelected = !binding.switchButton.isSelected
+            cameraFlashOnOff(binding.switchButton.isSelected)
+        }
+
+    }
+
+    private fun cameraFlashOnOff(isOn: Boolean) {
+        val cameraManager = applicationContext.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        if (isOn) {
+            cameraManager.setTorchMode(cameraId, true)
+        } else {
+            cameraManager.setTorchMode(cameraId, false)
+        }
     }
 }
